4be36d403cd4fa0615ebbf674bb77853e461ab0e
Dawid Szaniawski <www.dszaniawski@gmail.com>
2024-10-06 16:01:17 +0200

    chore: bump version

--- bepatient/__init__.py
+++ bepatient/__init__.py
@@ -14,7 +14,7 @@ from .waiter_src.checkers import CHECKERS
 from .waiter_src.checkers.checker import Checker
 from .waiter_src.comparators import COMPARATORS
 
-__version__ = "0.10.2"
+__version__ = "0.11.1"
 __all__ = [
     "Checker",
     "CHECKERS",
