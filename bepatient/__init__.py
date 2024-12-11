d8ed6b2d1daad36e31aff40f3645750f0f030567
Dawid Szaniawski <98256512+dawid-szaniawski@users.noreply.github.com>
2024-12-11 22:07:27 +0100

    feat(retry): add retry decorator (#23)

--- bepatient/__init__.py
+++ bepatient/__init__.py
@@ -10,16 +10,18 @@ from .api import (
     wait_for_value_in_request,
     wait_for_values_in_request,
 )
+from .retry import retry
 from .waiter_src.checkers import CHECKERS
 from .waiter_src.checkers.checker import Checker
 from .waiter_src.comparators import COMPARATORS
 
-__version__ = "0.12.0"
+__version__ = "0.13.0"
 __all__ = [
     "Checker",
     "CHECKERS",
     "COMPARATORS",
     "dict_differences",
+    "retry",
     "RequestsWaiter",
     "to_curl",
     "wait_for_values_in_request",
